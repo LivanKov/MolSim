0d3eb0e5dcde3aca84fa5f2060afcd9ebf807f75
Ivan Lomakov <loi@in.tum.de>
2024-11-05 21:29:00 +0100

    slight adjustements in order to include the class being tested

--- tests/hello_test.cc
+++ tests/hello_test.cc
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
-
+#include "ParticleContainer.h"
 
 TEST(HelloTest, BasicAssertions) {
+  ParticleContainer p{};
   EXPECT_STRNE("hello", "world");
   EXPECT_EQ(7 * 6, 42);
 }
