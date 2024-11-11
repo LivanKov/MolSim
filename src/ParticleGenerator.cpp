5b44e15652fa21c5882e8111d4a5fa0206d3cc51
Ivan <ilomakov4@gmail.com>
2024-11-11 20:48:50 +0100

    format

--- src/ParticleGenerator.cpp
+++ src/ParticleGenerator.cpp
@@ -1,52 +1,38 @@
-//
-// Created by sebastianpse on 11/9/24.
-//
+/*
+ * Created by sebastianpse on 11/9/24.
+ */
 
 #include "ParticleGenerator.h"
 #include "ParticleContainer.h"
 #include "utils/MaxwellBoltzmannDistribution.h"
 
-
 #include <random>
-
-
 
 ParticleGenerator::ParticleGenerator() = default;
 
-
 ParticleContainer ParticleGenerator::generateCuboid(
-    const std::array<double, 3>& lowerLeftFrontCorner,
-    const std::array<size_t, 3>& dimensions,
-    double h,
-    double m,
-    const std::array<double, 3>& initialVelocity,
-    double averageVelocity
-) {
-    ParticleContainer container;
-    for (size_t i = 0; i < dimensions[0]; ++i) {
-        for (size_t j = 0; j < dimensions[1]; ++j) {
-            for (size_t k = 0; k < dimensions[2]; ++k) {
-                std::array position = {
-                    lowerLeftFrontCorner[0] + i * h,
-                    lowerLeftFrontCorner[1] + j * h,
-                    lowerLeftFrontCorner[2] + k * h
-                };
+    const std::array<double, 3> &lowerLeftFrontCorner,
+    const std::array<size_t, 3> &dimensions, double h, double m,
+    const std::array<double, 3> &initialVelocity, double averageVelocity) {
+  ParticleContainer container;
+  for (size_t i = 0; i < dimensions[0]; ++i) {
+    for (size_t j = 0; j < dimensions[1]; ++j) {
+      for (size_t k = 0; k < dimensions[2]; ++k) {
+        std::array position = {lowerLeftFrontCorner[0] + i * h,
+                               lowerLeftFrontCorner[1] + j * h,
+                               lowerLeftFrontCorner[2] + k * h};
 
-
-                std::array<double, 3> velocity = initialVelocity;
-                std::array<double, 3> randomVelocity = maxwellBoltzmannDistributedVelocity(averageVelocity, 3);
-                for (size_t dim = 0; dim < 3; ++dim) {
-                    velocity[dim] += randomVelocity[dim];
-                }
-
-
-                Particle particle(position, velocity, m, 0);
-                container.insert(particle);
-            }
+        std::array<double, 3> velocity = initialVelocity;
+        std::array<double, 3> randomVelocity =
+            maxwellBoltzmannDistributedVelocity(averageVelocity, 3);
+        for (size_t dim = 0; dim < 3; ++dim) {
+          velocity[dim] += randomVelocity[dim];
         }
+
+        Particle particle(position, velocity, m, 0);
+        container.insert(particle);
+      }
     }
-    return container;
+  }
+  return container;
 }
-
-
-
--- src/ParticleGenerator.h
+++ src/ParticleGenerator.h
@@ -1,6 +1,6 @@
-//
-// Created by sebastianpse on 11/9/24.
-//
+/*
+ * Created by sebastianpse on 11/9/24.
+ */
 
 #pragma once
 
@@ -9,16 +9,11 @@
 
 class ParticleGenerator {
 public:
-    ParticleGenerator();
+  ParticleGenerator();
 
-    static ParticleContainer generateCuboid(
-        const std::array<double, 3>& lowerLeftFrontCorner,
-        const std::array<size_t, 3>& dimensions,
-        double h,
-        double mass,
-        const std::array<double, 3>& initialVelocity,
-        double averageVelocity
-    );
-
+  static ParticleContainer
+  generateCuboid(const std::array<double, 3> &lowerLeftFrontCorner,
+                 const std::array<size_t, 3> &dimensions, double h, double mass,
+                 const std::array<double, 3> &initialVelocity,
+                 double averageVelocity);
 };
-
