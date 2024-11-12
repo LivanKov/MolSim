572c069d2c5eda00c675a1e6462d2ffd600d9be8
Sebastian John <sebastian1107john@gmail.com>
2024-11-12 13:34:54 +0100

    added doxygen annotation to logger class

--- src/logger/Logger.cpp
+++ src/logger/Logger.cpp
@@ -1,39 +1,38 @@
 #include "Logger.h"
 
 Logger::Logger(const std::string &log_level) {
-  // Create a color console sink for coloured logs
-  auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-  console_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] %v");
-  console_sink->set_pattern("%+");
+    // Create a color console sink for coloured logs
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    console_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] %v");
+    console_sink->set_pattern("%+");
 
-  // Initialize the logger with the sink
-  logger = std::make_shared<spdlog::logger>("console", console_sink);
+    // Initialize the logger with the sink
+    logger = std::make_shared<spdlog::logger>("console", console_sink);
 
-  // Set the log level based on user input
-  if (log_level == "trace") {
-    logger->set_level(spdlog::level::trace);
-  } else if (log_level == "debug") {
-    logger->set_level(spdlog::level::debug);
-  } else if (log_level == "info") {
-    logger->set_level(spdlog::level::info);
-  } else if (log_level == "warn") {
-    logger->set_level(spdlog::level::warn);
-  } else if (log_level == "error") {
-    logger->set_level(spdlog::level::err);
-  } else if (log_level == "critical") {
-    logger->set_level(spdlog::level::critical);
-  } else if (log_level == "off") {
-    logger->set_level(spdlog::level::off);
-  } else {
-    logger->set_level(spdlog::level::info);
-  }
+    // Set the log level based on user input
+    if (log_level == "trace") {
+        logger->set_level(spdlog::level::trace);
+    } else if (log_level == "debug") {
+        logger->set_level(spdlog::level::debug);
+    } else if (log_level == "info") {
+        logger->set_level(spdlog::level::info);
+    } else if (log_level == "warn") {
+        logger->set_level(spdlog::level::warn);
+    } else if (log_level == "error") {
+        logger->set_level(spdlog::level::err);
+    } else if (log_level == "off") {
+        logger->set_level(spdlog::level::off);
+    } else {
+        logger->set_level(spdlog::level::info);
+    }
 }
 
-Logger::~Logger() { }
+Logger::~Logger() {
+}
 
 Logger &Logger::getInstance(const std::string &log_level) {
-  static Logger instance(log_level); // Ensures to be initialized only once
-  return instance;
+    static Logger instance(log_level); // Ensures to be initialized only once
+    return instance;
 }
 
 void Logger::trace(const std::string &message) { logger->trace(message); }
@@ -45,5 +44,3 @@ void Logger::info(const std::string &message) { logger->info(message); }
 void Logger::warn(const std::string &message) { logger->warn(message); }
 
 void Logger::error(const std::string &message) { logger->error(message); }
-
-void Logger::critical(const std::string &message) { logger->critical(message); }
