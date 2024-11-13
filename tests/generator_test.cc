8f982f78629db7cf663a7171711f65043350acfa
Ivan <ilomakov4@gmail.com>
2024-11-13 22:30:54 +0100

    added an additonal test to generate two bodies

--- tests/generator_test.cc
+++ tests/generator_test.cc
@@ -30,3 +30,40 @@ TEST_F(CuboidTest, SimpleTest) {
     }
   }
 }
+
+
+TEST_F(CuboidTest, MultipleCuboidsTest){
+  ParticleGenerator::insertCuboid(
+      std::array<double, 3>{0, 0, 0}, std::array<size_t, 3>{3, 3, 3}, 1.0, 1.0,
+      std::array<double, 3>{1.0, 1.0, 1.0}, 0, container);
+  ParticleGenerator::insertCuboid(
+      std::array<double, 3>{10, 10, 0}, std::array<size_t, 3>{3, 5, 4}, 1.0, 1.0,
+      std::array<double, 3>{1.0, 1.0, 1.0}, 0, container);
+
+  size_t index = 0;
+
+  for (size_t i = 0; i < 3; i++) {
+    for (size_t j = 0; j < 3; j++) {
+      for (size_t k = 0; k < 3; k++) {
+        Particle dummyParticle{std::array<double, 3>{static_cast<double>(k),
+                                                     static_cast<double>(j),
+                                                     static_cast<double>(i)},
+                               std::array<double, 3>{1.0, 1.0, 1.0}, 1.0};
+        ASSERT_EQ(container[index++], dummyParticle);
+      }
+    }
+  }
+
+  for (size_t i = 0; i < 4; i++) {
+    for (size_t j = 10; j < 5; j++) {
+      for (size_t k = 10; k < 3; k++) {
+        Particle dummyParticle{std::array<double, 3>{static_cast<double>(k),
+                                                     static_cast<double>(j),
+                                                     static_cast<double>(i)},
+                               std::array<double, 3>{1.0, 1.0, 1.0}, 1.0};
+        ASSERT_EQ(container[index++], dummyParticle);
+      }
+    }
+  }
+
+}
