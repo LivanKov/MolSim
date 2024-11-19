c8066a2d436be9f5206d5f76c155ba9fe2766d7c
Ivan Lomakov <ge95zoc@in.tum.de>
2024-11-19 18:34:32 +0100

    moved print help into parser class

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -43,10 +43,6 @@ void calculateV();
  */
 void plotParticles(int iteration);
 
-/*
- * print help flag
- */
-void print_help();
 
 // e : time-end
 // d : delta
@@ -150,26 +146,6 @@ int main(int argc, char *argsv[]) {
   return 0;
 }
 
-// ---------------------------------------------------------------------------------------------------------------------
-
-void print_help() {
-  std::cout << "Usage: MolSim [options]\n";
-  std::cout << "Options:\n";
-  std::cout << "  -h                 Show this help message\n";
-  std::cout << "  -o   <file_path>   Specify output file path\n";
-  std::cout << "  -i   <file_path>   Specify input file path\n";
-  std::cout
-      << "  -e   <end_time>    Specify how long the simulation should run\n";
-  std::cout << "  -d   <time_delta>  Specify time increments\n";
-  std::cout << "  -t                 Enable testing mode (Writes a file for "
-               "each iteration)\n";
-  std::cout << "  -x                 Output .xyz files instead of .vpu\n";
-  std::cout << "  -l   <log_level>   Option to choose the logging level "
-               "[trace, debug, info, warn, error, off]\n";
-  std::cout << "  -f                 Calculate Gravitational Force instead of "
-               "Lennard-Jones Force\n";
-  std::cout << "  -n   Disable all file output for the sake of performance\n";
-}
 
 void calculateF() {
   // store the current force as the old force and reset current to 0
--- src/utils/CommandParser.h
+++ src/utils/CommandParser.h
@@ -2,4 +2,23 @@ namespace CommandParser{
     void parse(int argc, char**argv, SimParams params){
 
     }
+
+    void print_help() {
+  std::cout << "Usage: MolSim [options]\n";
+  std::cout << "Options:\n";
+  std::cout << "  -h                 Show this help message\n";
+  std::cout << "  -o   <file_path>   Specify output file path\n";
+  std::cout << "  -i   <file_path>   Specify input file path\n";
+  std::cout
+      << "  -e   <end_time>    Specify how long the simulation should run\n";
+  std::cout << "  -d   <time_delta>  Specify time increments\n";
+  std::cout << "  -t                 Enable testing mode (Writes a file for "
+               "each iteration)\n";
+  std::cout << "  -x                 Output .xyz files instead of .vpu\n";
+  std::cout << "  -l   <log_level>   Option to choose the logging level "
+               "[trace, debug, info, warn, error, off]\n";
+  std::cout << "  -f                 Calculate Gravitational Force instead of "
+               "Lennard-Jones Force\n";
+  std::cout << "  -n   Disable all file output for the sake of performance\n";
+    }
 }
