e773b20b7933999348b7826184349dbf46454bec
LivanKov <ilomakov4@gmail.com>
2024-11-25 23:25:14 +0100

    default filewriter no longer abstract

--- src/io/output/FileWriter.cpp
+++ src/io/output/FileWriter.cpp
@@ -1,3 +1,6 @@
 #include "FileWriter.h"
 
 output::FileWriter::FileWriter(ParticleContainer& particles) : particles(particles) {};
+
+void output::FileWriter::plot_particles(const std::string& filepath, int iteration) {};
+    
--- src/io/output/FileWriter.h
+++ src/io/output/FileWriter.h
@@ -10,10 +10,8 @@ namespace output{
 class FileWriter {
 public:
     FileWriter(ParticleContainer& particles);
-
-    virtual ~FileWriter() = default;
     
-    virtual void plot_particles(const std::string& filepath, int iteration) = 0;
+    virtual void plot_particles(const std::string& filepath, int iteration);
 
     ParticleContainer& particles;
 };
