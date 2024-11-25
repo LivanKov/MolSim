214c903d5b8b1418358127827c00744c4db0e0be
LivanKov <ilomakov4@gmail.com>
2024-11-25 02:00:01 +0100

    remove unused imports

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -1,20 +1,7 @@
-#include "io/input/FileReader.h"
-#include "io/output/VTKWriter.h"
-#include "io/output/XYZWriter.h"
-#include "io/output/FileWriter.h"
-#include "simulator/particle/ParticleContainer.h"
-#include "utils/ArrayUtils.h"
-#include "simulator/particle/ParticleGenerator.h"
-#include <spdlog/spdlog.h>
 #include <iostream>
-#include "utils/logger/Logger.h"
+#include "simulator/Simulation.h"
 #include "utils/SimParams.h"
 #include "utils/CommandParser.h"
-#include "simulator/calculations/Calculation.h"
-#include "simulator/calculations/Force.h"
-#include "simulator/calculations/Position.h"
-#include "simulator/calculations/Velocity.h"
-#include "simulator/Simulation.h"
 
 
 
