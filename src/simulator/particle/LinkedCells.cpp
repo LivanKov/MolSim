16b0d2032a6df05702b2e4c05be5cd6c9e809371
LivanKov <ilomakov4@gmail.com>
2024-11-26 23:33:31 +0100

    added template specialization to only support 2 and 3 dimensional cells

---
+++ src/simulator/particle/LinkedCells.cpp
@@ -0,0 +1,10 @@
+#include "LinkedCells.h"
+#include <array>
+
+
+
+
+
+LinkedCells<3>::LinkedCells(std::array<double, 3> &domain_size, double r_cutoff) {
+  // ...
+}
--- src/simulator/particle/LinkedCells.h
+++ src/simulator/particle/LinkedCells.h
@@ -1,7 +1,30 @@
+#include "ParticleContainer.h"
+#include <array>
+
 #pragma once
 
+// 2 or 3 dimensions ???
 template <size_t N>
-class LinkedCells {
-public:
-  LinkedCells(std::array<double, N> &domain_size, double r_cutoff);
+struct Cell{
+    std::vector<ParticlePointer> particles;
+};
+
+
+
+
+
+template <size_t N>
+class LinkedCells {};
+
+
+template<>
+class LinkedCells<2> {
+    public:
+        LinkedCells(std::array<double,2>& domain_size, double r_cutoff);
+};
+
+template<>
+class LinkedCells<3> {
+    public:
+        LinkedCells(std::array<double,3>& domain_size, double r_cutoff);
 };
