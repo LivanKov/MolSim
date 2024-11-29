8f3d37d8f8f6b995733e0def19c06f882721606d
bangrui <wbr200076@gamil.com>
2024-11-29 22:37:10 +0100

    fixed log-level issue temporarily

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -1,10 +1,12 @@
 #include "io/input/cli/CommandParser.h"
 #include "io/input/cli/SimParams.h"
 #include "simulator/Simulation.h"
+#include "utils/logger/Logger.h"
 #include <iostream>
 
 int main(int argc, char *argsv[]) {
   SimParams parameters{};
+  Logger &logger = Logger::getInstance(argsv[2]);
   ParticleContainer particles = Simulation::readFile(argsv[1], parameters);
   SimParams overridedParams = CommandParser::parse(argc, argsv, parameters);
   auto simulation = Simulation::generate_simulation(overridedParams);
