e50fe8f3d1896777ec47f8ea021a4fcb5786ebe6
bangrui <wbr200076@gamil.com>
2024-12-19 23:21:01 +0100

    updated task2 examples. And two batch scripts

--- scripts/run_molsim_cm4tiny.sh
+++ scripts/run_molsim_cm4tiny.sh
@@ -3,17 +3,19 @@
 #SBATCH --output=MolSim_output.log   
 #SBATCH --error=MolSim_error.log      
 #SBATCH --get-user-env=L              
-#SBATCH --clusters=serial
-#SBATCH --partition=serial_std        
-#SBATCH --mem=8G 
-#SBATCH --cpus-per-task=1             
+#SBATCH --clusters=cm4
+#SBATCH --partition=cm4_tiny        
+#SBATCH --cpus-per-task=112             
 #SBATCH --export=NONE                
 #SBATCH --time=01:00:00 
 
 # Load required modules
+module load slurm_setup
 module load gcc/13.2.0                # GCC module for compilation
 module load cmake/3.26.3              # Ensure the correct version of CMake
 module load xerces-c                  # Xerces-C for XML processing
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 # Navigate to the build directory
 cd ~/MolSim/build
