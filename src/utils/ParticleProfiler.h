17c912a0da0921ac40e62b51c53503867c96b545
Sebastian John <sebastian1107john@gmail.com>
2025-01-30 22:27:34 +0100

    Added doxygen to ParticleProfiler

--- src/utils/ParticleProfiler.h
+++ src/utils/ParticleProfiler.h
@@ -7,17 +7,33 @@
 #include <iostream>
 #include <fstream>
 
-
-
-
+/**
+ *
+ * This class offers a component which divides the system into bins along the x-axis.
+ * It calculates the density and average velociy of the particles for each bin.
+ */
 class ParticleProfiler {
     public:
+    /**
+     * @brief Constructor
+     *
+     * @param particles: The particles of the system.
+     * @param number_of_bins: The number of bins in which the system should be divided.
+     * @param x_min: The x-point where the system of the bins should begin.
+     * @param x_max: The x-point where the system of the bins should end.
+     * @param output_file: The output file that gets written with the values
+     *
+     */
     ParticleProfiler(LinkedCellContainer &particles,
         size_t number_of_bins,
         double x_min,
         double x_max,
         const std::string& output_file);
 
+    /**
+     * @brief Writes the current state of the system to the output file.
+     *
+     */
     void apply_profiler();
 
 
@@ -28,6 +44,9 @@ class ParticleProfiler {
     double x_max_;
     double bin_width_;
     std::string output_file_;
+    /**
+     * @brief Checks if the header is written to ensure that the header is only written once.
+     */
     bool header_written_;
 
 
